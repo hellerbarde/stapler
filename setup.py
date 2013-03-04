6a35a55b407d35d42ed29d66cbf179546d335202
David Gay <oddshocks@gmail.com>
2013-03-04 13:37:57 -0500

    add setup.py and stuff so I can put stapler on pypi

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+version = '0.3.0'
+
+setup(name='stapler',
+    description="Manipulate PDF documents from the command line",
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Topic :: Utilities",
+    ],
+    keywords='pdf stapler cli',
+    version=version,
+    author='Fred Wenzel, Philip Stark',
+    url='https://github.com/hellerbarde/stapler',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'pypdf == 1.12',
+    ],
+    entry_points="""
+    [console_scripts]
+    stapler = staplelib:main
+    """,
+    )
+
