3b22f35de1eb755d555d08562140938ddc068cd5
John Heidemann <johnh@isi.edu>
2018-11-16 14:40:51 -0800

    add list-logical

--- staplelib/stapler.py
+++ staplelib/stapler.py
@@ -27,6 +27,8 @@ background: <inputfile> [<pagerange>[<rotation>]] ... (output needed)
     Merge/Overlay the given input files interleaved.
 info: <inputfile> ... (no output needed)
     Display PDF metadata
+list-log(ical): <inputfile>
+    Display the logical names of each page.
 
 Page ranges:
     n - single numbers mean single pages (e.g., 15)
@@ -76,6 +78,8 @@ def main():
         "info": commands.info,
         "zip": commands.zip,
         "background": commands.background,
+        "list-log": commands.list_logical_pages,
+        "list-logical": commands.list_logical_pages,
     }
 
     mode = args[0]
